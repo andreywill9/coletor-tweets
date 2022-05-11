d8e73792481abcc4d3d3b4ef6d4a5eedf0e226f6
andrey <andreywill9@gmail.com>
2022-05-11 19:04:47 -0300

    Buscando Tweets envolvendo os candidatos por palavra chave para cada candidato

--- main.py
+++ main.py
@@ -8,16 +8,26 @@ consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
+
+def concatenar_termos_candidato(entradas: list):
+    return ' OR '.join(map(lambda termo: termo.replace(' ', '%20'), entradas))
+
+
+def gerar_query(entradas: list):
+    return '(' + concatenar_termos_candidato(entradas) + ') lang:pt -has:links'
+
+
 formato_data = '%Y-%m-%dT%H:%M:01Z'
 data_inicial = (date.datetime.now() - date.timedelta(hours=4)).strftime(formato_data)
 data_final = date.datetime.now().strftime(formato_data)
 
 client = twitter.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret)
 
-candidatos = ['lula', 'bolsonaro', 'ciro', 'doria']
+candidatos = (['lula', 'Luiz Inacio Lula da Silva'], ['bolsonaro', 'Jair Messias Bolsonaro'], ['ciro', 'Ciro Gomes'],
+              ['tebet', 'Simone Tebet'], ['doria', 'Joao Doria'], ['janones', 'Andre Janones'])
 
-for candidato in candidatos:
-    query = candidato + " lang:pt -has:links"
+for entrada in candidatos:
+    query = gerar_query(entrada)
     tweets_buscados = client.search_recent_tweets(query=query, max_results=100,
                                                   start_time=data_inicial, end_time=data_final).data
     print("\n---------------------------------------------\n")
