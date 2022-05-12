aebaa491873ac886b8ced82e1a41a94034400d8c
andrey <andreywill9@gmail.com>
2022-05-11 21:49:19 -0300

    Utilizando Pandas para salvar dados coletados do Twitter em arquivo Excel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+
+import pandas as pd
 import tweepy as twitter
 import datetime as date
 
@@ -30,13 +32,32 @@ resultado_buscas = []
 
 for entrada in candidatos:
     query = gerar_query(entrada)
-    tweets_buscados = client.search_recent_tweets(query=query, max_results=100,
+    tweets_buscados = client.search_recent_tweets(query=query, max_results=10,
                                                   start_time=data_inicial, end_time=data_final,
                                                   tweet_fields=['author_id', 'created_at', 'public_metrics', 'source']).data
-    resultado_buscas.extend(tweets_buscados)
     for tweet in tweets_buscados:
         if tweet.author_id not in usuarios_buscados:
             usuarios_buscados[tweet.author_id] = client.get_user(id=tweet.author_id,
                                                                  user_fields=['created_at', 'verified',
                                                                               'public_metrics', 'location']).data
+        usuario = usuarios_buscados.get(tweet.author_id)
+        linha = [0 for j in range(13)]
+        linha[0] = tweet.id
+        linha[1] = tweet.created_at
+        linha[2] = tweet.text
+        linha[3] = tweet.public_metrics.get('retweet_count')
+        linha[4] = tweet.public_metrics.get('reply_count')
+        linha[5] = tweet.public_metrics.get('like_count')
+        linha[6] = tweet.source
+        linha[7] = usuario.id
+        linha[8] = usuario.username
+        linha[9] = usuario.name
+        linha[10] = usuario.created_at
+        linha[11] = usuario.public_metrics.get('followers_count')
+        linha[12] = usuario.verified
+        resultado_buscas.append(linha)
+dataframe = pd.DataFrame(resultado_buscas)
+dataframe.columns = ['id_tweet', 'data_tweet', 'texto', 'retweets', 'respostas', 'likes', 'fonte', 'id_usuario',
+                     'arroba_usuario', 'nome_usuario', 'data_criacao_usuario', 'seguidores_usuario', 'usuario_verificado']
+dataframe.to_excel('dados.xlsx')
 
